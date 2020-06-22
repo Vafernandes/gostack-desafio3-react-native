2ab26a6267544f8aac0d6c3a50a20a1c2c904017
Vafernandes <alvesbaterista.va@gmail.com>
2020-06-22 20:29:51 -0300

    Adionando as funções de deslike e deletar

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Atividade react-native GoStack
+
+### Esta atividade foi introdutória nos conceitos do react-native. O intuito do desafio era listar os repositórios juntamente com suas techs e incluir o famoso _Like_. Porém fui um pouco além disso, integrei as funcionalidades de deletar um repositório e adicionei o _delike_. Foi um bom exercício para começar a entender como o react-native funciona.
+
+#### Abaixo seguem as duas funções principais do desafio do GoStack
+
+```javascript
+
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            setRepositories(response.data);
+        })
+    }, [])
+
+    async function handleLikeRepository(id) {
+        const response = await api.post(`repositories/${id}/like`)
+        const findRepoByIndex = repositories.findIndex(repo => repo.id === id)
+        repositories[findRepoByIndex] = response.data;
+        setRepositories([...repositories]);
+    }
+
+```
+
+# Agora segue uma preview do aplicativo
+
+![](imgGif/a.gif)
--- src/App.js
+++ src/App.js
@@ -22,11 +22,22 @@ export default function App() {
 
   async function handleLikeRepository(id) {
     const response = await api.post(`repositories/${id}/like`)
-
     const findRepoByIndex = repositories.findIndex(repo => repo.id === id)
     repositories[findRepoByIndex] = response.data;
-
     setRepositories([...repositories]);
+  }
+
+  async function handleDeslikeRepository(id) {
+    const response = await api.post(`repositories/${id}/deslike`);
+    const findReposiotyIndex = repositories.findIndex(repository => repository.id === id);
+    repositories[findReposiotyIndex] = response.data;
+    setRepositories([...repositories]);
+  }
+
+  async function handleDelete(id) {
+    await api.delete(`repositories/${id}`)
+    const newRepository = repositories.filter(repository => repository.id !== id);
+    setRepositories(newRepository);
   }
 
   return (
@@ -56,15 +67,30 @@ export default function App() {
                   {repository.likes} curtida{repository.likes > 1 && <>s</>}
                 </Text>
               </View>
-
-              <TouchableOpacity
-                style={styles.button}
-                onPress={() => handleLikeRepository(repository.id)}
-                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-                testID={`like-button-${repository.id}`}
-              >
-                <Text style={styles.buttonText}>Curtir</Text>
-              </TouchableOpacity>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(repository.id)}
+                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                  testID={`like-button-${repository.id}`}
+                >
+                  <Text style={styles.buttonTextLike}>Curtir</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleDeslikeRepository(repository.id)}
+                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                  // testID={`like-button-${repository.id}`}
+                >
+                  <Text style={styles.buttonTextDelike}>Delike</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleDelete(repository.id)}
+                >
+                  <Text style={styles.buttonTextDelete}>Deletar</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           )}
         />
@@ -112,10 +138,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginRight: 10,
   },
+  buttonContainer: {
+    flexDirection: 'row'
+  },
   button: {
     marginTop: 10,
   },
-  buttonText: {
+  buttonTextLike: {
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
@@ -123,4 +152,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#7159c1",
     padding: 15,
   },
+  buttonTextDelete: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#e02041",
+    padding: 15,
+  },
+  buttonTextDelike: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#ec3",
+    padding: 15,
+  }
 });
